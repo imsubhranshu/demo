0a11cb00eaa67d949c7999d1b13502dc64b7451d
imsubhranshu <subhranshu.behera99.sb@gmail.com>
2021-04-13 12:05:05 +0530

    first commit

---
+++ MyClass.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Demo {
+    class MyClass {
+        public static void show() {
+            Console.WriteLine("Hello! This is MyClass");
+        }
+        public static int add(int a, int b) {
+            return a + b;
+        }
+        public static int subtract(int a, int b) {
+            return a - b;
+        }
+        public static int mul(int a, int b) {
+            return a * b;
+        }
+        public static double divide(int a, int b) {
+            return a / b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Enter a number: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Addition of {0}, {1} = {2}", a,b,MyClass.add(a,b));
+            Console.WriteLine("Subtraction of {0}, {1} = {2}", a,b,MyClass.subtract(a,b));
+            Console.WriteLine("Multiplicatioin of {0}, {1} = {2}", a,b,MyClass.mul(a,b));
+            Console.WriteLine("Division of {0}, {1} = {2}", a,b,MyClass.divide(a,b));
+        }
+    }
+}
